0d33a8e9d9fb28f340b7ee4b1f1d653c03d01fd0
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-01-20 12:25:15 +0300

    update main.jsx

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter> {/* Wrap App in HashRouter */}
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
